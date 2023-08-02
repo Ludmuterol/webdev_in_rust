bd32ccf3df1954b324a0a91907928a253522219b
tpeters <tpeters@student.42heilbronn.de>
2023-08-02 23:14:28 +0200

    pepper is attached to secret and not to salt

--- backend/src/crypto.rs
+++ backend/src/crypto.rs
@@ -8,19 +8,19 @@ const PEPPER_FILE: &[u8] = include_str!("../pepper.txt").as_bytes();
 pub fn encrypt(secret: String) -> (String, String) {
     let mut salt = [0u8; digest::SHA512_OUTPUT_LEN];
     rand::SystemRandom::new().fill(&mut salt).unwrap();
-    let mut salt_pepper: Vec<u8> = salt.to_vec();
-    salt_pepper.extend(PEPPER_FILE);
+    let mut peppered_secret: Vec<u8> = secret.as_bytes().to_vec();
+    peppered_secret.extend(PEPPER_FILE);
     let mut pbkdf2_hash = [0u8; digest::SHA512_OUTPUT_LEN];
-    pbkdf2::derive(pbkdf2::PBKDF2_HMAC_SHA512, NonZeroU32::new(210_000).unwrap(), &salt_pepper, secret.as_bytes(), &mut pbkdf2_hash);
+    pbkdf2::derive(pbkdf2::PBKDF2_HMAC_SHA512, NonZeroU32::new(210_000).unwrap(), &salt, &peppered_secret, &mut pbkdf2_hash);
     (HEXUPPER.encode(&salt), HEXUPPER.encode(&pbkdf2_hash))
 }
 
 pub fn verify(input: String, salt_encoded: &String, actual_hash: &String) -> Result<(), ()> {
     let salt = HEXUPPER.decode(salt_encoded.as_bytes()).unwrap();
-    let mut salt_pepper: Vec<u8> = salt.to_vec();
-    salt_pepper.extend(PEPPER_FILE);
+    let mut peppered_input: Vec<u8> = input.as_bytes().to_vec();
+    peppered_input.extend(PEPPER_FILE);
     let previously_derived = HEXUPPER.decode(actual_hash.as_bytes()).unwrap();
-    let res = pbkdf2::verify(pbkdf2::PBKDF2_HMAC_SHA512, NonZeroU32::new(210_000).unwrap(), &salt_pepper, input.as_bytes(), &previously_derived);
+    let res = pbkdf2::verify(pbkdf2::PBKDF2_HMAC_SHA512, NonZeroU32::new(210_000).unwrap(), &salt, &peppered_input, &previously_derived);
     match res {
         Ok(_) => Ok(()),
         Err(_) => Err(()),
