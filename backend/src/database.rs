9051d4843fca2aa1a899644f931a7dbb4bfd33c7
tpeters <tpeters@student.42heilbronn.de>
2023-10-16 01:50:27 +0200

    update surrealdb to v1.0.0

--- backend/src/database.rs
+++ backend/src/database.rs
@@ -8,8 +8,6 @@ use surrealdb::Surreal;
 
 use crate::crypto::{encrypt, new_session_id, verify};
 
-static DB: Surreal<Client> = Surreal::init();
-
 #[derive(Deserialize, Debug)]
 struct Record {
     #[allow(dead_code)]
@@ -23,48 +21,6 @@ pub struct LoginDatabaseEntry {
     salt: String,
 }
 
-pub async fn init() {
-    DB.connect::<Ws>("127.0.0.1:8001").await.unwrap();
-    DB.signin(Root {
-        username: "root",
-        password: "root",
-    })
-    .await
-    .unwrap();
-    DB.use_ns("base_ns").use_db("base_db").await.unwrap();
-}
-
-pub async fn query_username(ld: &LoginData) -> Vec<LoginDatabaseEntry> {
-    let mut result = DB
-        .query("SELECT * FROM type::table($table) WHERE username = $username")
-        .bind(("table", "logins"))
-        .bind(("username", ld.username.to_owned()))
-        .await
-        .unwrap();
-    result.take(0).unwrap()
-}
-
-pub async fn check_login_data(ld: &LoginData) -> Result<(), ()> {
-    let users = query_username(&ld).await;
-    match users.len() {
-        1 => match verify(&ld.password, &(users[0].salt), &(users[0].pw_hash)) {
-            Ok(_) => Ok(()),
-            Err(_) => Err(()),
-        },
-        _ => Err(()),
-    }
-}
-
-pub async fn create_new_login(ld: &LoginData) {
-    let (salt, pw_hash) = encrypt(&ld.password);
-    let entry = LoginDatabaseEntry {
-        username: ld.username.clone(),
-        pw_hash,
-        salt,
-    };
-    let _record: Record = DB.create("logins").content(entry).await.unwrap();
-}
-
 #[derive(Deserialize, Serialize)]
 pub struct SessionDatabaseEntry {
     pub sid: String,
@@ -72,35 +28,80 @@ pub struct SessionDatabaseEntry {
     pub expiration: Datetime,
 }
 
-pub async fn query_sid(sid: &String) -> Vec<SessionDatabaseEntry> {
-    let mut result = DB
-        .query("SELECT * FROM type::table($table) WHERE sid = $sid")
-        .bind(("table", "sessions"))
-        .bind(("sid", sid))
-        .await
-        .unwrap();
-    result.take(0).unwrap()
+pub struct DB {
+    db: Surreal<Client>,
 }
 
-pub async fn create_new_session(username: String) -> String {
-    clean_up_old_sessions().await;
-    let mut sid = new_session_id();
-    while query_sid(&sid).await.len() != 0 {
-        sid = new_session_id();
+impl DB {
+    pub async fn init() -> DB {
+        let tmp = DB {
+            db: Surreal::new::<Ws>("localhost:8001").await.unwrap()
+        };
+        tmp.db.signin(Root {
+            username: "root",
+            password: "root",
+        })
+        .await
+        .unwrap();
+        tmp.db.use_ns("base_ns").use_db("base_db").await.unwrap();
+        tmp
     }
-    let entry = SessionDatabaseEntry {
-        sid: sid.clone(),
-        username,
-        expiration: Duration(std::time::Duration::from_secs(10)) + Datetime(Utc::now()),
-    };
-    let _record: Record = DB.create("sessions").content(entry).await.unwrap();
-    sid
-}
-
-pub async fn clean_up_old_sessions() {
-    DB.query("DELETE type::table($table) WHERE expiration < $ts")
-        .bind(("table", "sessions"))
-        .bind(("ts", Datetime(Utc::now())))
-        .await
-        .unwrap();
+    pub async fn query_username(& self, ld: &LoginData) -> Vec<LoginDatabaseEntry> {
+        let mut result = self.db
+            .query("SELECT * FROM type::table($table) WHERE username = $username")
+            .bind(("table", "logins"))
+            .bind(("username", ld.username.to_owned()))
+            .await
+            .unwrap();
+        result.take(0).unwrap()
+    }
+    pub async fn check_login_data(& self, ld: &LoginData) -> Result<(), ()> {
+        let users = self.query_username(&ld).await;
+        match users.len() {
+            1 => match verify(&ld.password, &(users[0].salt), &(users[0].pw_hash)) {
+                Ok(_) => Ok(()),
+                Err(_) => Err(()),
+            },
+            _ => Err(()),
+        }
+    }
+    pub async fn create_new_login(& self, ld: &LoginData) {
+        let (salt, pw_hash) = encrypt(&ld.password);
+        let entry = LoginDatabaseEntry {
+            username: ld.username.clone(),
+            pw_hash,
+            salt,
+        };
+        let _record: Vec<LoginDatabaseEntry> = self.db.create("logins").content(entry).await.unwrap();
+    }
+    pub async fn query_sid(& self, sid: &String) -> Vec<SessionDatabaseEntry> {
+        let mut result = self.db
+            .query("SELECT * FROM type::table($table) WHERE sid = $sid")
+            .bind(("table", "sessions"))
+            .bind(("sid", sid))
+            .await
+            .unwrap();
+        result.take(0).unwrap()
+    }
+    pub async fn create_new_session(& self, username: String) -> String {
+        self.clean_up_old_sessions().await;
+        let mut sid = new_session_id();
+        while self.query_sid(&sid).await.len() != 0 {
+            sid = new_session_id();
+        }
+        let entry = SessionDatabaseEntry {
+            sid: sid.clone(),
+            username,
+            expiration: Duration(std::time::Duration::from_secs(10)) + Datetime(Utc::now()),
+        };
+        let _record: Vec<SessionDatabaseEntry> = self.db.create("sessions").content(entry).await.unwrap();
+        sid
+    }
+    pub async fn clean_up_old_sessions(& self) {
+        self.db.query("DELETE type::table($table) WHERE expiration < $ts")
+            .bind(("table", "sessions"))
+            .bind(("ts", Datetime(Utc::now())))
+            .await
+            .unwrap();
+    }
 }
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,12 +1,11 @@
 use chrono::Utc;
-use database::{create_new_session, query_sid};
 use pwned::api::*;
 use rocket::fs::NamedFile;
 use rocket::http::{Cookie, CookieJar, SameSite};
 use rocket::outcome::IntoOutcome;
 use rocket::request::FromRequest;
 use rocket::response::status::NotFound;
-use rocket::{get, launch, post, routes};
+use rocket::{get, launch, post, routes, State};
 use std::path::PathBuf;
 use surrealdb::sql::Datetime;
 
@@ -15,15 +14,16 @@ use common::{LoginData, ProfileData};
 mod crypto;
 mod database;
 
+use database::DB;
 #[post("/api/register", data = "<incoming>")]
-async fn register(incoming: String) -> String {
+async fn register(db: &State<DB>, incoming: String) -> String {
     match LoginData::from_str(&incoming) {
         Some(login) => {
             let login = LoginData {
                 username: login.username.to_lowercase(),
                 password: login.password,
             };
-            let list = database::query_username(&login).await;
+            let list = db.query_username(&login).await;
             match list.len() {
                 0 => {
                     let pwned = PwnedBuilder::default().build().unwrap();
@@ -33,7 +33,7 @@ async fn register(incoming: String) -> String {
                                 return "Error: This is a known Password!".to_owned();
                             }
                             false => {
-                                database::create_new_login(&login).await;
+                                db.create_new_login(&login).await;
                                 return "Ok".to_owned();
                             }
                         },
@@ -51,17 +51,17 @@ async fn register(incoming: String) -> String {
 }
 
 #[post("/api/login", data = "<incoming>")]
-async fn login(jar: &CookieJar<'_>, incoming: String) -> String {
+async fn login(db: &State<DB>, jar: &CookieJar<'_>, incoming: String) -> String {
     match LoginData::from_str(&incoming) {
         Some(login) => {
             let login = LoginData {
                 username: login.username.to_lowercase(),
                 password: login.password,
             };
-            let res = database::check_login_data(&login).await;
+            let res = db.check_login_data(&login).await;
             match res {
                 Ok(_) => {
-                    let mut c = Cookie::new("id", create_new_session(login.username).await);
+                    let mut c = Cookie::new("id", db.create_new_session(login.username).await);
                     c.set_secure(true);
                     c.set_http_only(true);
                     c.set_same_site(SameSite::Strict);
@@ -116,7 +116,7 @@ impl<'r> FromRequest<'r> for User {
     ) -> rocket::request::Outcome<User, Self::Error> {
         match request.cookies().get_private("id") {
             Some(cookie) => match cookie.value().parse::<String>().ok() {
-                Some(sid) => match query_sid(&sid).await {
+                Some(sid) => match request.rocket().state::<DB>().unwrap().query_sid(&sid).await {
                     i if i.len() == 1 => {
                         if i[0].expiration > Datetime(Utc::now()) {
                             Some(User)
@@ -140,14 +140,14 @@ fn secret(_user: User) -> String {
 }
 
 #[get("/api/profile")]
-async fn profile(_user: User, jar: &CookieJar<'_>) -> String {
+async fn profile(db: &State<DB>, _user: User, jar: &CookieJar<'_>) -> String {
     let var = jar
         .get_private("id")
         .unwrap()
         .value()
         .parse::<String>()
         .unwrap();
-    let i = query_sid(&var).await;
+    let i = db.query_sid(&var).await;
     ProfileData {
         username: i[0].username.to_owned(),
     }
@@ -157,17 +157,18 @@ async fn profile(_user: User, jar: &CookieJar<'_>) -> String {
 
 #[launch]
 async fn rocket() -> _ {
-    database::init().await;
-    rocket::build().mount(
-        "/",
-        routes![
-            index,
-            static_files,
-            login,
-            register,
-            logout,
-            secret,
-            profile
-        ],
-    )
+    rocket::build()
+        .manage(DB::init().await)
+        .mount(
+            "/",
+            routes![
+                index,
+                static_files,
+                login,
+                register,
+                logout,
+                secret,
+                profile
+            ],
+        )
 }
